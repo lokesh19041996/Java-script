4933151497c90b2e7ae254144780a7b860cad017
lokesh19041996 <sdlokesh234@gmail.com>
2022-08-10 10:20:33 +0530

    number

--- java-script-intro/sample.js
+++ java-script-intro/sample.js
@@ -3,3 +3,10 @@
 // 1.number, 2.string, 3. boolean,  4.undefined,  5.symbol , 6.null , 7.bigInt
 
 // 1.number 
+ 
+var a=10;
+var b=20;
+var c=30;
+var d=(b+c-a)
+console.log(d)
+console.log(typeof d)
