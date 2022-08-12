dfa242b412bedfb6c5fd7cec15bc558a5a1be4f0
lokesh19041996 <sdlokesh234@gmail.com>
2022-08-12 12:09:04 +0530

    for loop

--- IterativeStms/sample1.js
+++ IterativeStms/sample1.js
@@ -1,3 +1,11 @@
 for (let i = 0; i <= 10; i = i + 1) {
     console.log(i);
 }
+
+for (let i = 0; i <= 20; i = i + 2) {
+    console.log("even numbers :" + i)
+}
+
+for (let i = 1; i <= 20; i = i + 2) {
+    console.log("odd numbers :" + i)
+}
