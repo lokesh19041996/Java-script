4214b73a04309249b90d4a6a6748297b50982d58
lokesh19041996 <sdlokesh234@gmail.com>
2022-08-12 11:25:51 +0530

    if stmt

---
+++ coditionalStmts/one.js
@@ -0,0 +1,19 @@
+var empsal=45000
+console.log(empsal>50000);
+if(empsal>50000){
+    console.log("empsal");
+}
+
+// var sal = 25000
+// console.log(sal > 30000)
+// if (sal > 30000) {
+//     console.log("Eligible for All")
+// }
+
+// var flag = "rajini"
+// if (flag) {
+//     console.log("GM")
+// }
+// else {
+//     console.log("GN")
+// }
