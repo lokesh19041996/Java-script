43973b4968662c08288c98ede1ec96bfd33db91b
lokesh19041996 <sdlokesh234@gmail.com>
2022-08-13 13:36:59 +0530

    tables

--- IterativeStms/sample1.js
+++ IterativeStms/sample1.js
@@ -1,11 +1,29 @@
-for (let i = 0; i <= 10; i = i + 1) {
-    console.log(i);
-}
+// for (let i = 0; i <= 10; i = i + 1) {
+//     console.log(i);
+// }
 
-for (let i = 0; i <= 20; i = i + 2) {
-    console.log("even numbers :" + i)
-}
+// for (let i = 0; i <= 20; i = i + 2) {
+//     console.log("even numbers :" + i)
+// }
 
-for (let i = 1; i <= 20; i = i + 2) {
-    console.log("odd numbers :" + i)
+// for (let i = 1; i <= 20; i = i + 2) {
+//     console.log("odd numbers :" + i)
+// }
+
+// for (let i = 1; i <= 10; i = i + 1) {
+//     console.log("7 * " + i + " =" + 7 * i);
+// }
+
+
+// for (let i = 1; i <= 10; i = i + 1) {
+//     console.log("4 *" + i + "=" + 4 * i);
+// }
+
+
+for (let i = 1; i <= 20; i++) {
+    for (let j = 1; j <= 10; j++) {
+        console.log(i + "*" + j + "=" + i * j);
+        document.write(i + "*" + j + "=" + i * j + "<br>");
+
+    }
 }
