9774b7cf9bfc4770444bd940a6881127e0a67cff
lokesh19041996 <sdlokesh234@gmail.com>
2022-08-13 14:14:29 +0530

    task

---
+++ coditionalStmts/assignment.js
@@ -0,0 +1,29 @@
+// if a number is divisible by 7 or not
+
+/* let num =71
+if(num % 7 ==0){
+    document.write("num is divisible by 7")
+}
+else{
+    document.write("num is not divisible by 7")
+} */
+
+// if a number is multiple of 3 or not
+
+// var num
+// num = prompt('Enter the number')
+// if (num % 3 == 0) {
+//     document.write('Multiple of 3')
+// }
+// else {
+//     document.write('Not a multiple of 3')
+// }
+
+// var no = prompt("Enter the number");
+// if (no < 0) {
+//     document.write("You entered Negative Number.");
+// }
+// else {
+//     document.write("You entered Positive Number.");
+// }
+
