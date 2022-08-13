030e11df69a8ecf9981d69e856f8d38e5b0e07a6
lokesh19041996 <sdlokesh234@gmail.com>
2022-08-13 13:52:17 +0530

    by 2

--- coditionalStmts/two.js
+++ coditionalStmts/two.js
@@ -1,18 +1,18 @@
 // if a number is divisible by 7 or not
 
-let num = 14;
-if (num % 7 == 0) {
-    document.write("divisible by 7")
-}
-else{
-    document.write("not divisible by 7")
-}
-
-// if a number is divisible by 2
-// let num1 = 14;
-// if (num1 % 2 == 0) {
-//     document.write("divisible by 2")
+// let num = 14;
+// if (num % 7 == 0) {
+//     document.write("divisible by 7")
 // }
 // else{
-//     document.write("not divisible by 2")
+//     document.write("not divisible by 7")
 // }
+
+// if a number is divisible by 2
+let num1 = 21;
+if (num1 % 2 == 0) {
+    document.write("divisible by 2")
+}
+else{
+    document.write("not divisible by 2")
+}
