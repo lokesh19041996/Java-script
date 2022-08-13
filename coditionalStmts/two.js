96f43662d650b40c64719f8b1eeb7d6f309cd2c0
lokesh19041996 <sdlokesh234@gmail.com>
2022-08-13 13:55:11 +0530

    ok

--- coditionalStmts/two.js
+++ coditionalStmts/two.js
@@ -9,10 +9,19 @@
 // }
 
 // if a number is divisible by 2
-let num1 = 21;
-if (num1 % 2 == 0) {
-    document.write("divisible by 2")
+
+// let num1 = 21;
+// if (num1 % 2 == 0) {
+//     document.write("divisible by 2")
+// }
+// else{
+//     document.write("not divisible by 2")
+// }
+
+let num2 = 20
+if (num2 % 4 == 0) {
+    document.write("number is divisible by 4")
 }
-else{
-    document.write("not divisible by 2")
+else {
+    document.write("number is not divisible by 4")
 }
