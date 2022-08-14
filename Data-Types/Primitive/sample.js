0e3a93067199c33dbb9900e69dc8108bec4b533c
lokesh19041996 <sdlokesh234@gmail.com>
2022-08-14 15:27:38 +0530

    ok

--- Data-Types/Primitive/sample.js
+++ Data-Types/Primitive/sample.js
@@ -3,35 +3,48 @@
 // 1.number, 2.string, 3. boolean,  4.undefined,  5.symbol , 6.null , 7.bigInt
 
 // 1.number 
- 
-var a=10;
-var b=20;
-var c=30;
-var d=(b+c-a)
-console.log(d)
-console.log(typeof d)
+
+// var a=10;
+// var b=20;
+// var c=30;
+// var d=(b+c-a)
+// console.log(d)
+// console.log(typeof d)
 
 
-// 2. String
-var empname="lokesh"
-var emplocation="bangalore"
-console.log(empname)
-console.log(emplocation)
-console.log(typeof empname);
-console.log(typeof emplocation);
+// // 2. String
+// var empname="lokesh"
+// var emplocation="bangalore"
+// console.log(empname)
+// console.log(emplocation)
+// console.log(typeof empname);
+// console.log(typeof emplocation);
 
-// 3 Boolean
+// // 3 Boolean
 
-var bool=true
-var lean=false
-console.log(bool);
-console.log(lean);
+// var bool=true
+// var lean=false
+// console.log(bool);
+// console.log(lean);
 
-console.log(typeof bool);
-console.log(typeof lean);
+// console.log(typeof bool);
+// console.log(typeof lean);
 
-// 4 undefined
+// // 4 undefined
 
-var empemail;
-console.log(empemail);
-console.log(typeof empemail);
+// var empemail;
+// console.log(empemail);
+// console.log(typeof empemail);
+
+
+
+
+//logical
+
+let hour = 7
+
+if (hour < 10 || hour > 18) {
+    // console.log('The office is closed.');
+    document.write("The office is closed")
+}
+
